829eeb481b039bcd35ffbb6cd1e9a67392c8b820
RowDaBoat <reardenr@gmail.com>
2026-01-08 10:43:41 -0300

    chop show's output instead of chopping it

--- src/show.nim
+++ src/show.nim
@@ -11,27 +11,22 @@ import intsets
 import archetype
 import textTable
 
+
 type Spec*[T: tuple] = object
   discard
+
 
 proc show[T](component: T, maxWidth: int): seq[string] =
   for name, value in fieldPairs(component):
     var entry = name & ": " & $value
-    let chunk = min(entry.len, maxWidth)
-    result.add entry.substr(0, chunk)
-    entry = entry.substr(chunk)
-    let maxWidth = maxWidth - 2
 
-    while entry.len > 0:
-      let chunk = min(entry.len, maxWidth)
-      result.add "  " & entry.substr(0, chunk)
-      entry = entry.substr(chunk)
+    if entry.len > maxWidth:
+      entry = entry.substr(0, maxWidth - 3) & "..."
 
-proc getArchetypeTable[T: tuple](
-  world: var World,
-  archetype: Archetype,
-  maxWidth: int
-): seq[seq[seq[string]]] =
+    result.add entry
+
+
+proc getArchetypeTable[T: tuple](world: var World, archetype: Archetype, maxWidth: int): seq[seq[seq[string]]] =
   for name, typ in fieldPairs default T:
     let componentId = world.componentIdFrom typeof typ
 
@@ -42,6 +37,7 @@ proc getArchetypeTable[T: tuple](
         column.add(show(component, maxWidth))
 
       result.add @[ column ]
+
 
 proc show*[T: tuple](world: var World, tupleDesc: typedesc[T], maxWidth: int = 20): string =
   for archetype in world.archetypes:
