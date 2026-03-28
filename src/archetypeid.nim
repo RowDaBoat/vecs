54c0f3fcdc33498952118a50b42d76d9775340bd
Row <reardenr@gmail.com>
2026-03-28 14:37:47 -0300

    ArchetypeId is now a stack-allocated uint64 bitset

--- src/archetypeid.nim
+++ src/archetypeid.nim
@@ -1,17 +1,41 @@
-import std/hashes
+import std/[hashes, macros]
 import componentid
 
 
+func replace(node: NimNode, target, replacement: NimNode): NimNode =
+  if node.eqIdent(target):
+    return replacement
+
+  result = node.copyNimNode()
+
+  for child in node:
+    result.add child.replace(target, replacement)
+
+
+macro unroll*(idx: untyped, lo, hi: static int, body: untyped): untyped =
+  result = newNimNode(nnkStmtList)
+  for i in lo ..< hi:
+    result.add nnkBlockStmt.newTree(newEmptyNode(), body.replace(idx, newLit(i)))
+
+
+const ArchetypeWords* {.intdefine.} = 1
+static: assert ArchetypeWords > 0, "ArchetypeWords must be greater than 1"
+
+
 type ArchetypeId* = object
-  components: uint64
+  components: array[0..(ArchetypeWords - 1), uint64]
 
 
 proc incl*(self: var ArchetypeId, componentId: ComponentId) =
-  self.components = self.components or (1.uint64 shl componentId.uint64)
+  let word = componentId.int div 64
+  let bit = 1.uint64 shl (componentId.int mod 64)
+  self.components[word] = self.components[word] or bit
 
 
 proc excl*(self: var ArchetypeId, componentId: ComponentId) =
-  self.components = self.components and (not (1.uint64 shl componentId.uint64))
+  let word = componentId.int div 64
+  let bit = 1.uint64 shl (componentId.int mod 64)
+  self.components[word] = self.components[word] and (not bit)
 
 
 proc archetypeIdFrom*(componentIds: seq[ComponentId]): ArchetypeId =
@@ -20,15 +44,31 @@ proc archetypeIdFrom*(componentIds: seq[ComponentId]): ArchetypeId =
 
 
 proc contains*(self: ArchetypeId, componentId: ComponentId): bool =
-  (self.components and (1.uint64 shl componentId.uint64)) != 0
+  let word = componentId.int div 64
+  let bit = 1.uint64 shl (componentId.int mod 64)
+  (self.components[word] and bit) != 0
 
 
 proc contains*(self: ArchetypeId, archetypeId: ArchetypeId): bool =
-  (self.components and archetypeId.components) == archetypeId.components
+  result = true
+
+  unroll(i, 0, ArchetypeWords):
+    let left = self.components[i]
+    let right = archetypeId.components[i]
+
+    if (left and right) != right:
+      return false
 
 
 proc disjointed*(self: ArchetypeId, archetypeId: ArchetypeId): bool =
-  (self.components and archetypeId.components) == 0'u64
+  result = true
+
+  unroll(i, 0, ArchetypeWords):
+    let left = self.components[i]
+    let right = archetypeId.components[i]
+
+    if (left and right) != 0:
+      return false
 
 
 proc hash*(id: ArchetypeId): Hash =
